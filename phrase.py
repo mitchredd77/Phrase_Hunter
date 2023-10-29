2fbce199cb83cfc1ef5e74b2b99028aa24a07b5c
mitchredd77 <147752683+mitchredd77@users.noreply.github.com>
2023-10-29 11:17:19 -0600

    Create phrase.py

---
+++ phrase.py
@@ -0,0 +1,24 @@
+class Phrase:
+
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+    
+    def display(self, guesses):
+        for letter in self.phrase:
+            if letter in guesses:
+                 print(letter, end = " ")
+            else:
+                 print("_", end=" ")
+
+    def check_guess(self, guess):
+        self.guess = guess
+        if self.guess in self.phrase:
+            return True
+        else:
+            return False
+
+    def check_complete(self, guesses):
+        for letter in self.phrase:
+            if letter not in guesses:
+                return False
+
